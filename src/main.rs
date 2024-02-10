b16824b80ac5f4e542b8fcff84cd5c68a9077de3
Isaac Spencer <22ispencer@gmail.com>
2024-02-09 22:22:37 -0600

    added gtk startup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::prelude::*;
+use gtk::{glib, Application};
+
+const APP_ID: &str = "org.magma.pumice";
+
+fn main() -> glib::ExitCode  {
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Run the application
+    app.run()
 }
